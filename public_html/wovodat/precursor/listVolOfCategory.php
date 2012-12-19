c540b1c14d12785f210421f5ebe84ea52011d75e
root <root@ubuntu>
2012-12-19 11:03:46 -0500

    Revert "Delete useless files in the precursor folder."
    This reverts commit f5bd07fa50bc5ff66925ad3fe68daa466aaada7e.

---
+++ public_html/wovodat/precursor/listVolOfCategory.php
@@ -0,0 +1,29 @@
+<?php
+//listVolOfCategory.php
+	$listVolcanOfCategory=array(
+
+	'Stratovolcano'=>array("CampiFlegrei", "Vesuvius", "Stromboli", "Lipari", "Etna"),
+	'Basalticvolcano'=>array("Kialuea", "Palma", "Reunion", "Stromboli"),
+	'Andesiticvolcano'=>array("Merap", "Vesuvius", "Stromboli", "Lipari", "Etna"),
+	'Daciticvolcano'=>array("Pelee"),
+	'Lavadomevolcano'=>array("Chaines des Puys"),
+	'Maars'=>array("West Eifel Volcanic Field"),
+	'Pyroclasticconesvolcano'=>array("Olot Volcanic Field"),
+	'Shieldvolcano'=>array(""),
+	'Complexvolcano'=>array(""),
+	'Cinderconevolcano'=>array(""),
+	'Scoriaconevolcano'=>array(""),
+	'Pumiceconevolcano'=>array(""),
+	'Tuffconevolcano'=>array(""),
+	'Lavaconevolcano'=>array(""),
+	'Explosioncratervolcano'=>array(""),
+	'Fissureventvolcano'=>array(""),
+	'Compoundvolcano'=>array(""),
+	'Volcanicfield'=>array(""),
+	'Submarinevolcano'=>array(""),
+	'Volcanowithcraterlake'=>array("Ijen", "Ruapehu"),
+	'Volcanowithlavadome'=>array("Merapi", "Unzen", "Colima", "St. Helens"),
+	'Calderavolcano'=>array("Ijen", "Bromo", "Toba", "Batur", "Long Valley"),
+	'volcano'=>array("")
+	);
+?>
